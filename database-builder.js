3c90420cf594b3031179638d30bb69eaaf69c4ef
dcarbajal <daniel.carbajal@pucp.edu.pe>
2021-07-21 15:25:18 -0500

    last Update

--- database-builder.js
+++ database-builder.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const connectionString = 'mongodb+srv://newHorizonUser:newHorizonPassword@cluster0.8eh3p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const connectionString = 'mongodb+srv://daniel:macedonio@cluster0.axvxx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 async function getConnectedDatabase(databaseName) {
     const mongoClient = new MongoClient(connectionString, {
--- main.js
+++ main.js
@@ -2,9 +2,11 @@ const {getConnectedDatabase} = require('./database-builder')
 
 getConnectedDatabase('sample_weatherdata')
 .then(db => 
+
     db.collection('clase2')
         .find( {$or: [{ isAdmin: true }, {user:'Daniel'}]})
         .project({ user: true, _id: false })
+        
 )
 .then(x => x.toArray())
 .then(console.log)
