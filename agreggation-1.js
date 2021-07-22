03ec63c425e3b4666770fb12940d68cd6c70f95c
dcarbajal <daniel.carbajal@pucp.edu.pe>
2021-07-21 20:47:51 -0500

    agregations

--- agreggation-1.js
+++ agreggation-1.js
@@ -4,3 +4,12 @@ db.listingsAndReviews.aggregate([
     { $group: { _id: "$address.market", precioMedio : { $avg: "$price" }} },
     { $merge: { into: "PromediosUSA", on: "_id" } }
 ])
+
+
+db.listingsAndReviews.aggregate([
+    { $project : { "address.country":1, "address.market":1, amenities: 1 } },
+    { $match : { "address.country" : "United States"}},
+    { $unwind : "$amenities" },
+    { $group: { _id: "$amenities", cantidad : { $sum: 1 }} },
+    { $sort: { cantidad : -1 }}
+])
