d857a1aa237fb4beac60dcb731a537112edcd5ea
dcarbajal <daniel.carbajal@pucp.edu.pe>
2021-07-26 20:27:09 -0500

    LOOKUP

--- agregation-2.js
+++ agregation-2.js
@@ -28,3 +28,100 @@ db.listingsAndReviews.aggregate([
     { $group: { _id: "$pais", promedioDeCalentadores : { $avg: "$tieneCaracteristicaDeInteres" }} },
     { $sort: { promedioDeCalentadores : -1 }}
 ]);
+
+//EJERCICIOS SORT $sortByCount: 
+
+db.listingsAndReviews.aggregate([
+    { $sortByCount : "$property_type"}
+])
+
+db.listingsAndReviews.aggregate([
+    { $sortByCount : "$address.country"}
+])
+
+db.listingsAndReviews.aggregate([
+    { $unwind : "$reviews"},
+    { $sortByCount : "$reviews.reviewer_name"}
+])
+
+// BUCKET  { _id: 'Wifi', count: 5303 },
+db.listingsAndReviews.aggregate([ 
+    { $unwind: "$amenities"},
+    { $sortByCount: "$amenities"},
+    {
+        $bucket: {
+            groupBy : "$count",
+            boundaries: [ 1000,2000,3000,4000,5000],
+            default : "FueraDeRango",
+            output : {
+                total : { $sum: "$count" },
+                cantidad : { $sum: 1 },
+                amenities : {
+                    $push: {
+                        name: "$_id",
+                        quantity: "$count"
+                    }
+                }
+            }
+        }
+    }
+])
+
+
+ // EJERCICIOS BUCKET
+db.listingsAndReviews.aggregate([
+    {
+        $bucket: {
+            groupBy : "$price",
+            boundaries: [ 50,100,150,200],
+            default : "FueraDeRango",
+            output : {
+                cantidad : { $sum: 1 },
+                amenities : {
+                    $addToSet: "$price"
+                }
+            }
+        }
+    }
+])
+
+db.listingsAndReviews.aggregate([
+    { $project : { numeroDeAmenities : { $size: "$amenities"}} },
+    {
+        $bucket: {
+            groupBy : "$numeroDeAmenities",
+            boundaries: [ 5,10,15,20,30],
+            default : "FueraDeRango",
+            output : {
+                cantidad : { $sum: 1 },
+                amenities : {
+                    $addToSet: "$numeroDeAmenities"
+                }
+            }
+        }
+    }
+])
+
+// UNION ALL
+
+db.neighborhoods.aggregate([
+    { $project : { _id: 0, name:1, type: "neighborhood" } },
+    { $unionWith: { coll: "restaurants", pipeline: [ { $project : { _id: 0, name:1, type: "restaurant" }}  ] }},
+    { sort : { type : 1 }}
+])
+
+// Aggregacion tipo JOIN
+
+db.transactions.aggregate([
+    { $project : { transactions : 0 }},
+    {  
+        $lookup: {
+            from : 'accounts',
+            localField: "account_id",
+            foreignField: "account_id",
+            as: "account"
+        }
+    }
+])
+
+
